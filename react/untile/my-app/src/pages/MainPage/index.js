d74fd99a18a4aae476a907ea7084b8a62daa759f
pedrogilsenarego <pedrogilsenarego@gmail.com>
2022-02-14 21:43:25 +0000

    a

--- react/untile/my-app/src/pages/MainPage/index.js
+++ react/untile/my-app/src/pages/MainPage/index.js
@@ -9,6 +9,7 @@ import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
 import InputLabel from "@mui/material/InputLabel";
 import ButtonGroup from "@mui/material/ButtonGroup";
+import Divider from "@mui/material/Divider";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -22,16 +23,12 @@ const mapState = (state) => ({
 	conversion: state.conversionData.conversion
 });
 
-const SUPPORTED_CURRENCIES =
-	"https://api.coingecko.com/api/v3/simple/supported_vs_currencies";
-
 const MainPage = () => {
 	const { conversion } = useSelector(mapState);
 	const [supportedCurrencies, setSupportedCurrencies] = useState([]);
 	const [selectedCurrency, setSelectedCurrency] = useState("");
-
+	const [currentPage, setCurrentPage] = useState(0);
 	const [targetCurrency, setTargetCurrency] = useState("");
-
 	const [market, setMarket] = useState("");
 	const [selectedMarket, setSelectedMarket] = useState("");
 	const [warning, setWarning] = useState("");
@@ -45,7 +42,9 @@ const MainPage = () => {
 
 	const getData = async () => {
 		try {
-			const response = await axios.get(SUPPORTED_CURRENCIES);
+			const response = await axios.get(
+				"https://api.coingecko.com/api/v3/simple/supported_vs_currencies"
+			);
 			const data = response.data;
 			setSupportedCurrencies(data);
 		} catch (error) {
@@ -169,174 +168,226 @@ const MainPage = () => {
 
 	return (
 		<div>
-			<Container style={{ backgroundColor: "lightblue", marginTop: "10vh" }}>
-				<Box style={{ backgroundColor: "black", color: "white" }}>
-					Welcome to the Crypto CUrrency Converter
-				</Box>
-				<Box style={{ backgroundColor: "lightGrey", marginTop: "10vh" }}>
-					<Typography>Convert here any currency</Typography>
-					<Grid container spacing={2} style={{ marginTop: "10vh" }}>
-						<Grid item container xs={6}>
-							<Grid item xs={6}>
-								<FormControl>
-									<InputLabel id="initial currency">Currency</InputLabel>
-									<Select
-										labelId="initial currency"
-										id="initial currency"
-										value={selectedCurrency}
-										label="Currency pretended"
-										onChange={handleChangeCurrency}
-										style={{ minWidth: "200px" }}
-									>
-										{supportedCurrencies.map((item, pos) => {
-											return (
-												<MenuItem key={pos} value={item}>
-													{item}
-												</MenuItem>
-											);
-										})}
-									</Select>
-								</FormControl>
-							</Grid>
-							<Grid item xs={6}>
-								<TextField
-									placeholder="Initial value"
-									value={initialInput}
-									onChange={(e) => setInitialInput(e.target.value)}
-								></TextField>
-							</Grid>
-							<Grid item xs={6} style={{ marginTop: "20px" }}>
-								<FormControl>
-									<InputLabel id="target currency">Currency</InputLabel>
-									<Select
-										labelId="target currency"
-										id="target currency"
-										value={targetCurrency}
-										label="Currency pretended"
-										onChange={handleTargetCurrency}
-										style={{ minWidth: "200px" }}
-									>
-										{supportedCurrencies.map((item, pos) => {
-											return (
-												<MenuItem key={pos} value={item}>
-													{item}
-												</MenuItem>
-											);
-										})}
-									</Select>
-								</FormControl>
-							</Grid>
-							<Grid item xs={6} style={{ marginTop: "20px" }}>
-								<TextField
-									disabled={true}
-									placeholder="Final value"
-									value={initialInput * rate}
-								></TextField>
-							</Grid>
-							<Grid
-								item
-								xs={12}
-								style={{ marginTop: "20px", marginBottom: "20px" }}
-							>
-								<ButtonGroup>
-									<Button onClick={() => handleDoConversion()}>Convert</Button>
-									<Button onClick={() => handleSaveConversion()}>
-										Save Conversion
-									</Button>
-									<Button onClick={() => handleClearConversions()}>
-										Clear conversions
-									</Button>
-								</ButtonGroup>
-								{warning}
-							</Grid>
-						</Grid>
-						<Grid item xs={6}>
-							<Typography>List of Conversions done</Typography>
-							<div style={{ marginTop: "20px" }}>
-								{conversion.map((item, pos) => {
-									return (
-										<Typography key={pos}>
-											{pos + 1}: {item.initialValue} of {item.initialCurrency}{" "}
-											converts to {item.finalValue} of {item.secondaryCurrency}
-										</Typography>
-									);
-								})}
-							</div>
-						</Grid>
-					</Grid>
-				</Box>
-
-				<Box
-					style={{
-						backgroundColor: "lightGrey",
-						marginTop: "10vh",
-						minHeight: "50vh"
-					}}
+			<Container style={{ backgroundColor: "lightGrey", marginTop: "10vh" }}>
+				<Grid
+					container
+					spacing={2}
+					direction="row"
+					alignItems="center"
+					justifyContent="center"
 				>
-					<Typography>Explore here the market</Typography>
-					<Grid container spacing={2} style={{ marginTop: "10vh" }}>
-						{" "}
-						<Grid item xs={6}>
-							<FormControl>
-								<InputLabel id="target coins">Coins</InputLabel>
-								<Select
-									labelId="target coins"
-									id="target coins"
-									value={selectedCoin}
-									label="Currency pretended"
-									onChange={handleSelectCoins}
-									style={{ minWidth: "200px" }}
+					<Grid
+						item
+						style={{
+							display: "flex",
+							color: "darkBlue"
+						}}
+					>
+						<Typography
+							onClick={() => {
+								setCurrentPage(0);
+							}}
+							style={{ cursor: "pointer", fontWeight: "600", fontSize: "20px" }}
+						>
+							{" "}
+							Crypto Calculator
+						</Typography>
+					</Grid>
+					<Grid
+						item
+						style={{
+							display: "flex",
+							color: "darkBlue"
+						}}
+					>
+						<Typography
+							onClick={() => {
+								setCurrentPage(1);
+							}}
+							style={{ cursor: "pointer", fontWeight: "600", fontSize: "20px" }}
+						>
+							{" "}
+							Tickers
+						</Typography>
+					</Grid>
+					<Divider
+						style={{
+							width: "100%",
+							marginTop: "15px",
+							background: "lightGrey"
+						}}
+					/>
+				</Grid>
+				{currentPage === 0 && (
+					<Box style={{ backgroundColor: "lightGrey", marginTop: "10vh" }}>
+						<Typography>Convert here any currency</Typography>
+						<Grid container spacing={2} style={{ marginTop: "10vh" }}>
+							<Grid item container xs={6}>
+								<Grid item xs={6}>
+									<FormControl>
+										<InputLabel id="initial currency">Currency</InputLabel>
+										<Select
+											labelId="initial currency"
+											id="initial currency"
+											value={selectedCurrency}
+											label="Currency pretended"
+											onChange={handleChangeCurrency}
+											style={{ minWidth: "200px" }}
+										>
+											{supportedCurrencies.map((item, pos) => {
+												return (
+													<MenuItem key={pos} value={item}>
+														{item}
+													</MenuItem>
+												);
+											})}
+										</Select>
+									</FormControl>
+								</Grid>
+								<Grid item xs={6}>
+									<TextField
+										placeholder="Initial value"
+										value={initialInput}
+										onChange={(e) => setInitialInput(e.target.value)}
+									></TextField>
+								</Grid>
+								<Grid item xs={6} style={{ marginTop: "20px" }}>
+									<FormControl>
+										<InputLabel id="target currency">Currency</InputLabel>
+										<Select
+											labelId="target currency"
+											id="target currency"
+											value={targetCurrency}
+											label="Currency pretended"
+											onChange={handleTargetCurrency}
+											style={{ minWidth: "200px" }}
+										>
+											{supportedCurrencies.map((item, pos) => {
+												return (
+													<MenuItem key={pos} value={item}>
+														{item}
+													</MenuItem>
+												);
+											})}
+										</Select>
+									</FormControl>
+								</Grid>
+								<Grid item xs={6} style={{ marginTop: "20px" }}>
+									<TextField
+										disabled={true}
+										placeholder="Final value"
+										value={initialInput * rate}
+									></TextField>
+								</Grid>
+								<Grid
+									item
+									xs={12}
+									style={{ marginTop: "20px", marginBottom: "20px" }}
 								>
-									{coins &&
-										coins.map((d) => (
-											<MenuItem key={d.name} value={d.name}>
-												{d.name}
-											</MenuItem>
-										))}
-								</Select>
-							</FormControl>
+									<ButtonGroup>
+										<Button onClick={() => handleDoConversion()}>
+											Convert
+										</Button>
+										<Button onClick={() => handleSaveConversion()}>
+											Save Conversion
+										</Button>
+										<Button onClick={() => handleClearConversions()}>
+											Clear conversions
+										</Button>
+									</ButtonGroup>
+									{warning}
+								</Grid>
+							</Grid>
+							<Grid item xs={6}>
+								<Typography>List of Conversions done</Typography>
+								<div style={{ marginTop: "20px" }}>
+									{conversion.map((item, pos) => {
+										return (
+											<Typography key={pos}>
+												{pos + 1}: {item.initialValue} of {item.initialCurrency}{" "}
+												converts to {item.finalValue} of{" "}
+												{item.secondaryCurrency}
+											</Typography>
+										);
+									})}
+								</div>
+							</Grid>
 						</Grid>
-						<Grid item xs={6}>
-							<FormControl>
-								<InputLabel id="target market">Market</InputLabel>
-								<Select
-									disabled={handleDisableMarket()}
-									labelId="target market"
-									id="target market"
-									value={selectedMarket}
-									label="Market pretended"
-									onChange={handleMarket}
-									style={{ minWidth: "200px" }}
-								>
-									{market.tickers &&
-										market.tickers.map((item, pos) => {
-											return (
-												<MenuItem value={item.market.name} key={pos}>
-													{item.market.name}
+					</Box>
+				)}
+				{currentPage === 1 && (
+					<Box
+						style={{
+							backgroundColor: "lightGrey",
+							marginTop: "10vh",
+							minHeight: "50vh"
+						}}
+					>
+						<Typography>Explore here the market</Typography>
+						<Grid container spacing={2} style={{ marginTop: "10vh" }}>
+							{" "}
+							<Grid item xs={6}>
+								<FormControl>
+									<InputLabel id="target coins">Coins</InputLabel>
+									<Select
+										labelId="target coins"
+										id="target coins"
+										value={selectedCoin}
+										label="Currency pretended"
+										onChange={handleSelectCoins}
+										style={{ minWidth: "200px" }}
+									>
+										{coins &&
+											coins.map((d) => (
+												<MenuItem key={d.name} value={d.name}>
+													{d.name}
 												</MenuItem>
-											);
-										})}
-								</Select>
-							</FormControl>
+											))}
+									</Select>
+								</FormControl>
+							</Grid>
+							<Grid item xs={6}>
+								<FormControl>
+									<InputLabel id="target market">Market</InputLabel>
+									<Select
+										disabled={handleDisableMarket()}
+										labelId="target market"
+										id="target market"
+										value={selectedMarket}
+										label="Market pretended"
+										onChange={handleMarket}
+										style={{ minWidth: "200px" }}
+									>
+										{market.tickers &&
+											market.tickers.map((item, pos) => {
+												return (
+													<MenuItem value={item.market.name} key={pos}>
+														{item.market.name}
+													</MenuItem>
+												);
+											})}
+									</Select>
+								</FormControl>
+							</Grid>
 						</Grid>
-					</Grid>
-					<Grid item xs={12} style={{ marginTop: "20px" }}>
-						{tickers.tickers &&
-							selectedMarket !== "" &&
-							tickers.tickers.map((item, pos) => {
-								if (item.market.name === selectedMarket) {
-									return (
-										<Typography key={pos}>
-											Ticker:{item.base} Last value:{item.last} Last Traded at:{" "}
-											{item.last_traded_at} Market: {item.market.name} Market
-											volume:
-											{item.volume}
-										</Typography>
-									);
-								} else return null;
-							})}
-					</Grid>
-				</Box>
+						<Grid item xs={12} style={{ marginTop: "20px" }}>
+							{tickers.tickers &&
+								selectedMarket !== "" &&
+								tickers.tickers.map((item, pos) => {
+									if (item.market.name === selectedMarket) {
+										return (
+											<Typography key={pos}>
+												Ticker:{item.base} Last value:{item.last} Last Traded
+												at: {item.last_traded_at} Market: {item.market.name}{" "}
+												Market volume:
+												{item.volume}
+											</Typography>
+										);
+									} else return null;
+								})}
+						</Grid>
+					</Box>
+				)}
 			</Container>
 		</div>
 	);
